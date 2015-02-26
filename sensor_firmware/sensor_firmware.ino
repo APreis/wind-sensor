8173640b2b8e2988e5fb5bdd7b17625eb578c90a
Kenny Luong <luong97@hawaii.edu>
2015-02-26 13:06:19 -1000

    Fixed firware - remember to import the lib!

--- sensor_firmware/sensor_firmware.ino
+++ sensor_firmware/sensor_firmware.ino
@@ -6,23 +6,37 @@
 
 #include "TimerOne.h"
 
+
+int counter = 0;
+int average = 0;
+
 void setup() {
-    int baud_rate = 115200;
+    int baud_rate = 9600;
     Serial.begin(baud_rate);
 
     // Set sample period in microseconds for 8kHz
     long timer_delay_us = 125;
-    Timer.initialize(timer_delay_us);
+    Timer1.initialize(timer_delay_us);
+    Timer1.attachInterrupt(interrupt_loop);
 }
 
 void loop() {
     // We don't need to do anything for now
 }
 
+/* Here is the routine that is run when the timer interrupt goes off */
 void interrupt_loop(){
-    // Sample sensor
     int sensorValue = analogRead(A0);
-    Serial.println(sensorValue);
+
+    // Prints out every 5000 samples
+    if(counter == 5000){
+        Serial.println(sensorValue);
+        counter = 0;
+    }
+    else{
+        average = (average + sensorValue)/2;
+        counter++;
+    }
 }
 
 
