b9fbac3de73c121d3ba4b5b898312ba17a07ebac
Kenny Luong <luong97@hawaii.edu>
2015-03-03 22:02:48 -1000

    Added filtering code

--- sensor_firmware/sensor_firmware.ino
+++ sensor_firmware/sensor_firmware.ino
@@ -8,34 +8,46 @@
 
 int counter = 0;
 int average = 0;
+float output = 0;
+float previous_output = 0;
+int freq = 4000;
 
 void setup() {
     int baud_rate = 9600;
     Serial.begin(baud_rate);
+    Serial.println("Begin program");
+    // analogReference(EXTERNAL);
 
-    // Set sample period in microseconds for 8kHz
-    long timer_delay_us = 125;
+    // Set frequency
+    long period = ((float)1.0/(float)freq)*(1E6);
+    Serial.print("Period: ");
+    Serial.println(period);
+    long timer_delay_us = period;
+
+    // Attach timer
     Timer1.initialize(timer_delay_us);
     Timer1.attachInterrupt(interrupt_loop);
 }
 
 void loop() {
-    // We don't actually need anything in here since all of our 
-    // logic is in the Interrupt Service Routine (ISR)
 }
 
 /* Here is the routine that is run when the timer interrupt goes off */
 void interrupt_loop(){
     int sensorValue = analogRead(A0);
 
-    // prints out an average value after 5000 samples
-    // TODO: Implement our actual filter here
-    if(counter == 5000){
-        Serial.println(sensorValue);
+    // Print out every second
+    if(counter >= freq){
+        previous_output = output;
+        output = output*0.190 + 2.753;
+        // output = 2.753;
+        Serial.println(output);
         counter = 0;
     }
+    // Sample continiously
     else{
-        average = (average + sensorValue)/2;
+        output = 0.0002*abs(sensorValue - 508) + 0.9998*(previous_output);
+        previous_output = output;
         counter++;
     }
 }
